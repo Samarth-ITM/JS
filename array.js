6ba138f90c49fb1f042094636e9da7d15d746c1c
RougeParrot <rougeparrot@gmail.com>
2026-01-22 14:01:49 +0530

    J22.1

--- array.js
+++ array.js
@@ -1,12 +1,12 @@
-st = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
-mrk = [10, 9, 8 , 7 , 6, 5 , 4, 3, 2 ,1]
+st = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+mrk = [10, 9, 8 , 7 , 6, 5 , 4, 3, 2 ,1];
 
-sum = 0
+sum = 0;
 
 for (let i of mrk){
-    sum += i
+    sum += i;
 }
 
-avg = sum/mrk.length
+avg = sum/mrk.length;
 
-console.log(avg)
+console.log(avg);
