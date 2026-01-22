dd738fc11ff2a19b7ad9392cf861cb5e914c8e8a
RougeParrot <rougeparrot@gmail.com>
2026-01-22 19:04:51 +0530

    org

--- array.js
+++ array.js
@@ -1,6 +1,9 @@
 st = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 mrk = [10, 9, 8 , 7 , 6, 5 , 4, 3, 2 ,1];
-
+st.push("K");
+mrk.unshift(11);
+console.log(st)
+console.log(mrk)
 sum = 0;
 
 for (let i of mrk){
