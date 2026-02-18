c348ef0dd98dbc5f261aead49c5a7a5750746c70
RougeParrot <rougeparrot@gmail.com>
2026-02-18 11:02:30 +0530

    m

--- JSMiniProject/script.js
+++ JSMiniProject/script.js
@@ -2,6 +2,5 @@ const btn = document.querySelector(".ai-assistant-btn");
 const chat = document.querySelector(".chatbox");
 
 btn.addEventListener("click", function(){
-    chat.style.display =
-        chat.style.display === "flex" ? "none" : "flex";
+    chat.style.display = chat.style.display === "flex" ? "none" : "flex";
 });
